0505d1537622e8d88718e1a23f57b9f3b1871ab1
shladamo19 <aadamo1919@gmail.com>
2025-06-17 18:44:40 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import EnergyDashboard from './screens/EnergyDashboard';
-
-const Stack = createNativeStackNavigator();
+import DashboardEnergy from './DashboardEnergy';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Dashboard" component={EnergyDashboard} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <DashboardEnergy />;
 }
