dae6ced0f269806d4e46f3d66fc80b5ce58d761c
shladamo19 <aadamo1919@gmail.com>
2025-06-17 20:05:29 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import DashboardEnergy from './DashboardEnergy';
+import EnergyDashboard from './EnergyDashboard';
 
 export default function App() {
-  return <DashboardEnergy />;
+  return <EnergyDashboard />;
 }
